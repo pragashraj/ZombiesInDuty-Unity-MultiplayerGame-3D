99f8105f554db38ea53a68df10f45760573e6b68
pragashraj <rajpragashraj30@gmail.com>
2021-09-25 11:29:10 +0530

    feat: map

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -7,11 +7,16 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject objectCompletion;
     [SerializeField] private Text completionMessage;
     [SerializeField] private Animation coverPanel;
+    [SerializeField] private GameObject mapUI;
+    [SerializeField] private Transform mapCam;
+    [SerializeField] private float scale;
+
+    private bool isMapOpen;
 
     private bool objective1Completed;
     private bool objective2Completed;
     private bool objective3Completed;
-    private bool objective4Completed = true;
+    private bool objective4Completed;
     private bool objective5Completed;
     private bool objective6Completed;
 
@@ -21,6 +26,29 @@ public class GameManager : MonoBehaviour
     public bool Objective4Completed { get => objective4Completed; set => objective4Completed = value; }
     public bool Objective5Completed { get => objective5Completed; set => objective5Completed = value; }
     public bool Objective6Completed { get => objective6Completed; set => objective6Completed = value; }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            isMapOpen = !isMapOpen;
+        }
+
+        mapUI.SetActive(isMapOpen);
+
+        HandleMapScroll();
+    }
+
+
+    private void HandleMapScroll()
+    {
+        if (isMapOpen)
+        {
+            float scroll = Input.mouseScrollDelta.y;
+            mapCam.Translate(0, scroll * scale, 0, Space.Self);
+        }
+    }
 
     public void HandleCompletionUI(string message)
     {
