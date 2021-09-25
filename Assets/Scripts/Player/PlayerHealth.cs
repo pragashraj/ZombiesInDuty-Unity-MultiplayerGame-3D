9edbed245fd8aab603efe870f7852c1a622c8f2b
pragashraj <rajpragashraj30@gmail.com>
2021-09-25 08:42:43 +0530

    feat: player health script created

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,20 +1,20 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ActionLoader : MonoBehaviour
+public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private Image content;
     [SerializeField] private float lerpSpeed;
 
     [Range(0, 100)]
-    private float fillAmount = 0;
+    private float health = 100;
 
-    public float FillAmount { get => fillAmount; set => fillAmount = value; }
-
+    public float Health { get => health; set => health = value; }
+    
     void Update()
     {
-        float amount = Map(FillAmount, 100, 1);
-        content.fillAmount = amount;
+        float amount = Map(Health, 100, 1);
+        content.fillAmount = Mathf.Lerp(content.fillAmount, amount, Time.deltaTime * lerpSpeed);
     }
 
     private float Map(float value, float max, float min)
